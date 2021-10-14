a13c918d8d5ede03da0c8db4d1e4e421c957442f
Студенты Черепановой А.О <cherao@teach.local>
2021-10-14 14:21:09 +0300

    first commit

---
+++ Bank/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bank
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
